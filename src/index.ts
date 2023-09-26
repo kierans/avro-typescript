7331e1b954e855f2b10b5d47d6057ef3a77cc388
Kieran Simpson <Kieran.Simpson@anz.com>
2023-09-26 10:00:01 +1000

    Code clean up

--- src/index.ts
+++ src/index.ts
@@ -48,9 +48,19 @@ export function convertPrimitive(avroType: string): string {
 /** Converts an Avro record type to a TypeScript file */
 export function avroToTypeScript(schema: Schema, opts: ConversionOptions = {}): string {
     const output: string[] = [];
-    if (isEnumType(schema)) convertEnum(schema, output);
-    else if (isRecordType(schema)) convertRecord(schema, output, opts);
-    else throw "Unknown top level type " + (schema as unknown)["type"];
+
+    if (isEnumType(schema)) {
+        convertEnum(schema, output);
+    }
+    else {
+        if (isRecordType(schema)) {
+            convertRecord(schema, output, opts);
+        }
+        else {
+            throw "Unknown top level type " + (schema as unknown)["type"];
+        }
+    }
+
     return output.join("\n");
 }
 
@@ -76,31 +86,45 @@ export function convertType(type: Type, buffer: string[], opts: ConversionOption
     // if it's just a name, then use that
     if (typeof type === "string") {
         return convertPrimitive(type) || type;
-    } else if (type instanceof Array) {
+    }
+
+    if (type instanceof Array) {
         // array means a Union. Use the names and call recursively
         return type.map((t) => convertType(t, buffer, opts)).join(" | ");
-    } else if (isRecordType(type)) {
-        //} type)) {
+    }
+
+    if (isRecordType(type)) {
         // record, use the name and add to the buffer
         return convertRecord(type, buffer, opts);
-    } else if (isArrayType(type)) {
+    }
+
+    if (isArrayType(type)) {
         // array, call recursively for the array element type
         return convertType(type.items, buffer, opts) + "[]";
-    } else if (isMapType(type)) {
+    }
+
+    if (isMapType(type)) {
         // Dictionary of types, string as key
         return `{ [index:string]:${convertType(type.values, buffer, opts)} }`;
-    } else if (isEnumType(type)) {
+    }
+
+    if (isEnumType(type)) {
         // array, call recursively for the array element type
         return convertEnum(type, buffer);
-    } else if (isLogicalType(type)) {
+    }
+
+    if (isLogicalType(type)) {
         if (opts.logicalTypes && opts.logicalTypes[type.logicalType]) {
             return opts.logicalTypes[type.logicalType];
         }
+
         return convertType(type.type, buffer, opts);
-    } else {
-        console.error("Cannot work out type", type);
-        return "UNKNOWN";
     }
+
+
+    console.error("Cannot work out type", type);
+
+    return "UNKNOWN";
 }
 
 export function convertFieldDec(field: Field, buffer: string[], opts: ConversionOptions): string {
